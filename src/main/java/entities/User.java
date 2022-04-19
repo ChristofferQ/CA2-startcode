5f26d8d637d67e775e1722ab41bbfcb90f7bab7c
Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
2022-04-19 11:52:25 +0200

    first commit

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,97 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.mindrot.jbcrypt.BCrypt;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @Basic(optional = false)
+  @NotNull
+  @Column(name = "user_name", length = 25)
+  private String userName;
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 255)
+  @Column(name = "user_pass")
+  private String userPass;
+  @Column(name = "user_salt")
+  private String userSalt;
+  @JoinTable(name = "user_roles", joinColumns = {
+    @JoinColumn(name = "user_name", referencedColumnName = "user_name")}, inverseJoinColumns = {
+    @JoinColumn(name = "role_name", referencedColumnName = "role_name")})
+  @ManyToMany
+  private List<Role> roleList = new ArrayList<>();
+
+  public List<String> getRolesAsStrings() {
+    if (roleList.isEmpty()) {
+      return null;
+    }
+    List<String> rolesAsStrings = new ArrayList<>();
+    roleList.forEach((role) -> {
+        rolesAsStrings.add(role.getRoleName());
+      });
+    return rolesAsStrings;
+  }
+
+  public User() {}
+
+  //TODO Change when password is hashed
+   public boolean verifyPassword(String userPass){
+
+    return(BCrypt.checkpw(userPass,this.userPass));
+    }
+
+  public User(String userName, String userPass) {
+    this.userName = userName;
+    this.userSalt = BCrypt.gensalt(10);
+    this.userPass = encrypt(userPass);
+  }
+
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getUserPass() {
+    return this.userPass;
+  }
+
+  public void setUserPass(String userPass) {
+    this.userPass = userPass;
+  }
+
+  public List<Role> getRoleList() {
+    return roleList;
+  }
+
+  public void setRoleList(List<Role> roleList) {
+    this.roleList = roleList;
+  }
+
+  public void addRole(Role userRole) {
+    roleList.add(userRole);
+  }
+
+  private String encrypt (String password) { return BCrypt.hashpw(password,this.userSalt); }
+
+}
